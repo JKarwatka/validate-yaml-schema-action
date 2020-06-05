2ab9a4c97764db30a8f7849cb06af05c4995e951
Jakub Karwatka <jakubkarwatka@gmail.com>
2020-06-05 23:25:46 +0200

    Split code into separate files

--- src/index.js
+++ src/index.js
@@ -1,38 +1,8 @@
 import * as core from '@actions/core'
-import * as glob from 'glob'
-import * as fs from 'fs'
-import { validateYamlFile } from './validateYamlFile'
 
 
+core.debug('HELLO THERE')
 
-const validateYamlFilesInDirectory = ({path, schema}) =>{
-
-  const pathStats = fs.lstatSync(path)
-  if(pathStats.isDirectory()){
-    glob(`${process.env.GITHUB_WORKSPACE}/${path}/**/*.yaml`, {}, (err, files) => {
-      files.map(validateYamlFile)
-    })
-
-  }
-  if(pathStats.isFile()){
-    validateFile(path)
-  }
-}
-
-const main = () =>{
-  try {
-    core.debug('GITHUB_WORKSPACE: ' + process.env.GITHUB_WORKSPACE)
-    const filesToValidate = core.getInput('files-to-validate');
-    //TODO
-    //check if filesToValidate is and Array with correct structure
-    const filesList = JSON.parse(filesToValidate)
-
-    filesList.map(validateYamlFilesInDirectory)
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
-}
 
 
 
